ae4494c44e4e807ccec1ba02b27a886c7fd3bd8a
Crist Ye <iamCristYe@outlook.com>
2020-02-08 21:57:49 +0800

    Update CheckImmdStatus.user.js

--- CheckImmdStatus.user.js
+++ CheckImmdStatus.user.js
@@ -23,13 +23,13 @@
     let Birthday_year = "";
 
     if (document.getElementsByClassName("formBox")[3] != undefined) {
-        if (document.getElementsByClassName("formBox")[3].innerText.indexOf("You may proceed with the online service without these functions") != -1 || document.getElementsByClassName("formBox")[3].innerText.indexOf("可選擇繼續使用本項服務") != -1) {
+        if (document.getElementsByClassName("formBox")[3].innerText.indexOf("You may proceed with the online service without these functions") != -1 || document.getElementsByClassName("formBox")[3].innerText.indexOf("使用本") != -1) {
             document.getElementsByClassName("buttonExit")[0].click();
         }
     }
 
     if (document.getElementsByClassName("formBoxWithPadding")[0] != undefined) {
-        if (document.getElementsByClassName("formBoxWithPadding")[0].innerText.indexOf("You may use this service to enquire the application status") != -1 || document.getElementsByClassName("formBoxWithPadding")[0].innerText.indexOf("你可使用此項服務查詢有關香港簽證") != -1) {
+        if (document.getElementsByClassName("formBoxWithPadding")[0].innerText.indexOf("You may use this service to enquire the application status") != -1 || document.getElementsByClassName("formBoxWithPadding")[0].innerText.indexOf("使用此") != -1) {
             document.getElementsByClassName("buttondefault")[0].click();
         }
     }
@@ -41,7 +41,7 @@
     }
 
     if (document.getElementsByTagName("td")[3] != undefined) {
-        if (document.getElementsByTagName("td")[3].innerText.indexOf("Applicant's date of birth") == -1 || document.getElementsByTagName("td")[3].innerText.indexOf("申請人的出生日期") == -1 || document.getElementsByTagName("td")[3].innerText.indexOf("申請人的出生日期") == -1) {
+        if (document.getElementsByTagName("td")[3].innerText.indexOf("Applicant's date of birth") == -1 && document.getElementsByTagName("td")[3].innerText.indexOf("出生日期") == -1) {
             document.getElementsByTagName("input")[0].value = Application_reference_number_part1;
             document.getElementsByTagName("input")[1].value = Application_reference_number_part2;
             document.getElementsByTagName("input")[2].value = Application_reference_number_part3;
